4dc67c64557e90d334f49863dad93eaab729d6b7
BondeJohan <johan.andersson3086@outlook.com>
2021-10-18 12:45:55 +0200

    Added Methods

--- Program.cs
+++ Program.cs
@@ -16,17 +16,17 @@ namespace Calculator
             while (runApp)
             {
 
-                /*
+                
                 Console.WriteLine("Please enter your name.");
                 string name = Console.ReadLine();
-                */
+                
                 Console.WriteLine("Please enter the first number.");
                 double num1 = Convert.ToDouble(Console.ReadLine());
                 
                 Console.WriteLine("Please enter the second number.");
                 double num2 = Convert.ToDouble(Console.ReadLine());
                                 
-                Console.WriteLine("Choose which operation you want to do"); // {0}.", name);
+                Console.WriteLine("Choose which operation you want to do {0}.", name);
                 Console.WriteLine("1. Addition");
                 Console.WriteLine("2. Subtraction");
                 Console.WriteLine("3. Multiplication");
@@ -34,60 +34,11 @@ namespace Calculator
                 int choice = Convert.ToInt32(Console.ReadLine());
 
                 double result = SelectOperation(num1, num2, choice);
-                previousCalculation.Add(SaveCalculations(num1, num2, result, choice));
                 
-                /*
-                 
-                if (choice == 1)
-                {
-                    Console.WriteLine("Your result: {0} + {1} = {2}", num1, num2, result);
-                    string addToList = string.Format("{0} + {1} = {2}", num1, num2, result);
-                    previousCalculation.Add(addToList);
-                    
-                }
+                previousCalculation.Add(PrintAndSaveCalculations(num1, num2, result, choice));
 
-                else if (choice == 2)
-                {
-                    Console.WriteLine("Your result: {0} - {1} = {2}", num1, num2, result);
-                    string addToList = string.Format("{0} - {1} = {2}", num1, num2, result);
-                    previousCalculation.Add(addToList);
-                }
+                ContinueSeePreviousOrStop(name);
 
-                else if (choice == 3)
-                {
-                    Console.WriteLine("Your result: {0} * {1} = {2}", num1, num2, result);
-                    string addToList = string.Format("{0} * {1} = {2}", num1, num2, result);
-                    previousCalculation.Add(addToList);
-                }
-
-                else if (choice == 4)
-                {
-                    Console.WriteLine("Your result: {0} / {1} = {2}", num1, num2, result);
-                    string addToList = string.Format("{0} / {1} = {2}", num1, num2, result);
-                    previousCalculation.Add(addToList);
-                }
-                
-                 */
-                
-                                
-                Console.WriteLine("1. Do more calculations.");
-                Console.WriteLine("2. See your previous caclulations.");
-                Console.WriteLine("3. Close Calculator");
-                int stopChoice = Convert.ToInt32(Console.ReadLine());
-                                
-
-                if (stopChoice == 2)
-                {
-                    foreach (string lastCalculation in previousCalculation)
-                    {
-                        Console.WriteLine("Previous Operation: {0}", lastCalculation);
-                    }
-                }
-
-                else if (stopChoice == 3)
-                {
-                    runApp = false;
-                }
             }
 
             double SelectOperation(double num1, double num2, int choice)
@@ -116,36 +67,71 @@ namespace Calculator
                 return result;
             }
 
-            string SaveCalculations(double num1, double num2, double result, int choice)
+            string PrintAndSaveCalculations(double num1, double num2, double result, int choice)
             {
                 string addToList = "";
 
                 if (choice == 1)
                 {
+                    Console.WriteLine("Your result: {0} + {1} = {2}", num1, num2, result);
                     addToList = string.Format("{0} + {1} = {2}", num1, num2, result);
                 }
 
                 else if (choice == 2)
-                {                    
+                {
+                    Console.WriteLine("Your result: {0} - {1} = {2}", num1, num2, result);
                     addToList = string.Format("{0} - {1} = {2}", num1, num2, result);                    
                 }
 
                 else if (choice == 3)
-                {                    
+                {
+                    Console.WriteLine("Your result: {0} * {1} = {2}", num1, num2, result);
                     addToList = string.Format("{0} * {1} = {2}", num1, num2, result);                    
                 }
 
                 else if (choice == 4)
-                {                    
-                    addToList = string.Format("{0} / {1} = {2}", num1, num2, result);                    
+                {
+                    Console.WriteLine("Your result: {0} / {1} = {2}", num1, num2, result);
+                    addToList = string.Format("{0} / {1} = {2}", num1, num2, result);
                 }
 
                 return addToList; 
             }
 
-            void PrintCalculations()
+            void ContinueSeePreviousOrStop(string name)
             {
+                Console.WriteLine("1. Do more calculations.");
+                Console.WriteLine("2. See your previous caclulations.");
+                Console.WriteLine("3. Close Calculator");
+                int stopChoice = Convert.ToInt32(Console.ReadLine());
 
+
+                if (stopChoice == 2)
+                {
+                    PrintSavedCalculations();
+                    ContinueSeePreviousOrStop(name);
+                }
+
+                else if (stopChoice == 3)
+                {
+                    StopApp(name);
+                }
+            }
+
+            void PrintSavedCalculations()
+            {
+                foreach (string lastCalculation in previousCalculation)
+                {
+                    Console.WriteLine("Previous Operation: {0}", lastCalculation);
+                }
+            }
+
+            bool StopApp(string name)
+            {
+                runApp = false;
+
+                Console.WriteLine("Goodbye {0}", name);
+                return runApp;
             }
         }
     }
