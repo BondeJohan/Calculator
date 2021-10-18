4846e8d64100bcc889a9109cb29bf769474200ca
BondeJohan <johan.andersson3086@outlook.com>
2021-10-18 13:53:36 +0200

    Added more methods

--- Program.cs
+++ Program.cs
@@ -24,78 +24,53 @@ namespace Calculator
                 double num1 = Convert.ToDouble(Console.ReadLine());
                 
                 Console.WriteLine("Please enter the second number.");
-                double num2 = Convert.ToDouble(Console.ReadLine());
+                double num2 = Convert.ToDouble(Console.ReadLine());                               
+
+
+                SelectOperation(num1, num2, name);
+                ContinueSeePreviousOrStop(name);
+
+            }
+
+            void SelectOperation(double num1, double num2, string name)
+            {
                                 
                 Console.WriteLine("Choose which operation you want to do {0}.", name);
                 Console.WriteLine("1. Addition");
                 Console.WriteLine("2. Subtraction");
                 Console.WriteLine("3. Multiplication");
-                Console.WriteLine("4. Division");                
+                Console.WriteLine("4. Division");
                 int choice = Convert.ToInt32(Console.ReadLine());
-
-                double result = SelectOperation(num1, num2, choice);
-                
-                previousCalculation.Add(PrintAndSaveCalculations(num1, num2, result, choice));
-
-                ContinueSeePreviousOrStop(name);
-
-            }
-
-            double SelectOperation(double num1, double num2, int choice)
-            {
-                double result = 0;
+                double result;
 
                 if (choice == 1)
                 {
                     result = num1 + num2;
+                    Console.WriteLine("Your result: {0} + {1} = {2}", num1, num2, result);
+                    previousCalculation.Add(string.Format("{0} + {1} = {2}", num1, num2, result));
                 }
 
                 else if (choice == 2)
                 {
                     result = num1 - num2;
+                    Console.WriteLine("Your result: {0} - {1} = {2}", num1, num2, result);
+                    previousCalculation.Add(string.Format("{0} - {1} = {2}", num1, num2, result));
                 }
 
                 else if (choice == 3)
                 {
                     result = num1 * num2;
+                    Console.WriteLine("Your result: {0} * {1} = {2}", num1, num2, result);
+                    previousCalculation.Add(string.Format("{0} * {1} = {2}", num1, num2, result));
                 }
 
                 else if (choice == 4)
                 {
                     result = num1 / num2;
-                }
-                return result;
-            }
-
-            string PrintAndSaveCalculations(double num1, double num2, double result, int choice)
-            {
-                string addToList = "";
-
-                if (choice == 1)
-                {
-                    Console.WriteLine("Your result: {0} + {1} = {2}", num1, num2, result);
-                    addToList = string.Format("{0} + {1} = {2}", num1, num2, result);
-                }
-
-                else if (choice == 2)
-                {
-                    Console.WriteLine("Your result: {0} - {1} = {2}", num1, num2, result);
-                    addToList = string.Format("{0} - {1} = {2}", num1, num2, result);                    
-                }
-
-                else if (choice == 3)
-                {
-                    Console.WriteLine("Your result: {0} * {1} = {2}", num1, num2, result);
-                    addToList = string.Format("{0} * {1} = {2}", num1, num2, result);                    
-                }
-
-                else if (choice == 4)
-                {
                     Console.WriteLine("Your result: {0} / {1} = {2}", num1, num2, result);
-                    addToList = string.Format("{0} / {1} = {2}", num1, num2, result);
+                    previousCalculation.Add(string.Format("{0} / {1} = {2}", num1, num2, result));
                 }
-
-                return addToList; 
+                
             }
 
             void ContinueSeePreviousOrStop(string name)
